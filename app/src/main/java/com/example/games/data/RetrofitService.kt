cbc5a0be86b1df6a47a29b4f9f8ed9f4126ed004
AraceliLG <lozznorma30@gmail.com>
2024-08-21 10:23:50 -0600

    firts fixes

--- app/src/main/java/com/example/games/data/RetrofitService.kt
+++ app/src/main/java/com/example/games/data/RetrofitService.kt
@@ -7,3 +7,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Header
 import retrofit2.http.Query;
 
+interface RetrofitService {
+
+    @GET("movie/now_playing")
+    suspend fun listPopularMovies(
+        @Query("language") language: String,
+        @Header("Authorization") apiKey: String,
+        @Header("accept") accept: String,
+    ): RemoteResult
+}
