4246dbf2c7b6651d09dcb287abb712dcdf70114e
AraceliLG <lozznorma30@gmail.com>
2024-08-21 10:21:40 -0600

    student

--- app/src/main/java/com/example/games/data/Student.kt
+++ app/src/main/java/com/example/games/data/Student.kt
@@ -2,3 +2,16 @@ package com.example.games.data
 
 import com.example.games.R
 
+data class Student(
+    val nombre: String,
+//    val grado: String,
+//    val grupo: String,
+    val foto: Int
+)
+
+// Información de los integrantes
+val students = listOf(
+    Student("Norma Araceli López Gómez", R.drawable.a),
+    Student("Miriam Olayne Gómez López", R.drawable.c),
+    Student("Iván Rubisel Gutiérrez Cruz", R.drawable.b)
+)
